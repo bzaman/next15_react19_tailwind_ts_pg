b7c4b1d748d11688d1c7a31499e43442e49b344c
Badiuzzaman <folkgraphein@gmail.com>
2025-07-03 23:52:44 +0600

    Nextjs 15 with tailwind initialize and clean up

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Geist } from "next/font/google";
+import "@/assets/styles/globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+export const metadata: Metadata = {
+  title: "G-Store",
+  description: "A Modern ecomerce platform built with Next.js",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
